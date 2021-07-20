1ec8a23069d90c78a3786639cc056dbb0788d78d
pavanyalala <pavanyalala4508@gmail.com>
2021-07-20 11:02:35 +0530

    [pavan]: Add program initialized

---
+++ Demo/AddSub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    class AddSub
+    {
+        public void Add()
+        {
+            Console.Write("Enter first Number :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine("Addition of two numbers : " + sum);
+        }
+
+      
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddSub addsub = new AddSub();
+            addsub.Add();
+
+        }
+    }
+}
+
